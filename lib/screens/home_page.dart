31dea0c3a54d6641bc78878ec7b423a36ea13815
Riandec <studpitch65@gmail.com>
2025-10-13 05:40:55 +0700

    Update Home Page

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,7 +10,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int index = 0;
+  int navIndex = 0, genreIndex = 0, picIndex = 0;
+  final CarouselSliderController _titleController = CarouselSliderController();
+  final CarouselSliderController _imageController = CarouselSliderController();
+
   final Color active = Color.fromRGBO(0, 85, 255, 1);
   final Color inactive = Colors.white;
 
@@ -17,10 +21,18 @@ class _HomePageState extends State<HomePage> {
   final List<double> labelDx = [0, 0, 0, 0]; // + move to the right, - move to the left
   final double labelDy = 0; // + move down, - move up
 
-  Widget label(String label, int idx) {
-    final selected = index == idx;
-    return Text(label, style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: selected ? active : inactive));
-  }
+  final List<String> genres = const ['All', 'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Inspirational', 'Strategy', 'Thriller'];
+
+  final List<String> images = const [
+    '../assets/images/the-boy-who-cried-wolf.jpg',
+    '../assets/images/the-hare-and-the-tortoise.jpg',
+    '../assets/images/the-fox-and-the-grapes.jpg'
+  ];
+  final List<String> titles = const [
+    'The Boy Who Cried Wolf',
+    'The Hare and the Tortoise',
+    'The Fox and the Grapes'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -35,13 +47,13 @@ class _HomePageState extends State<HomePage> {
             buttonBackgroundColor: Colors.black,
             backgroundColor: Colors.transparent,
             items: [
-              Icon(Icons.home, size: 30 ,color: index == 0 ? active : Colors.white),
-              Icon(Icons.search, size: 30 ,color: index == 1 ? active : Colors.white),
-              Icon(Icons.book, size: 30 ,color: index == 2 ? active : Colors.white),
-              Icon(Icons.settings, size: 30 ,color: index == 3 ? active : Colors.white),
+              Icon(Icons.home, size: 30 ,color: navIndex == 0 ? active : inactive),
+              Icon(Icons.search, size: 30 ,color: navIndex == 1 ? active : inactive),
+              Icon(Icons.book, size: 30 ,color: navIndex == 2 ? active : inactive),
+              Icon(Icons.settings, size: 30 ,color: navIndex == 3 ? active : inactive),
             ],
-            index: index,
-            onTap: (i) => setState(() => index = i),
+            index: navIndex,
+            onTap: (i) => setState(() => navIndex = i),
           ),
           Positioned(
             left: 0,
@@ -58,7 +70,14 @@ class _HomePageState extends State<HomePage> {
                         offset: Offset(
                           i < labelDx.length ? labelDx[i] : 0, labelDy,
                         ),
-                        child: label(labels[i], i),
+                        child: Text(
+                          labels[i], 
+                          style: TextStyle(
+                            fontSize: 12, 
+                            fontWeight: FontWeight.bold, 
+                            color: navIndex == i ? active : inactive 
+                          )
+                        ),
                       ),
                     ),
                   );
@@ -69,6 +88,8 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: Container(
+        width: double.infinity,
+        height: double.infinity,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
@@ -79,6 +100,122 @@ class _HomePageState extends State<HomePage> {
               Colors.white,
             ]
           ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 20, left: 20),
+              child: Text(
+                "Hi, little one\nLet's explore our stories",
+                style: TextStyle(
+                  fontFamily: 'Darumadrop One',
+                  fontSize: 32,
+                  height: 1.25
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+
+            // Genre Selection
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: Container(
+                height: 30,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: genres.length,
+                  itemBuilder: (context, index) {
+                    bool isSelected = genreIndex == index;
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          genreIndex = index;
+                        });
+                      },           
+                      child: Container(
+                        margin: EdgeInsets.only(right: 10),
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(20),
+                          color: isSelected ? active : inactive,
+                        ),
+                        child: Center(
+                          child: Text(
+                            genres[index], 
+                            style: TextStyle(
+                              fontSize: 12, 
+                              fontWeight: FontWeight.bold, 
+                              color: isSelected ? Colors.white : Colors.black
+                            )
+                          )
+                        )
+                      ),
+                    );
+                  }
+                ),
+              )
+            ),
+            SizedBox(height: 30),
+
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Carousel for Images
+                  CarouselSlider(
+                    carouselController: _imageController,
+                    items: images.map((item) => Container(
+                      margin: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(image: AssetImage(item), fit: BoxFit.cover)
+                      ),
+                    )).toList(), 
+                    options: CarouselOptions(
+                      height: 350,
+                      enlargeCenterPage: true,
+                      viewportFraction: 0.5,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          picIndex = index;
+                        });
+                        _titleController.animateToPage(index);
+                      }
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  // Carousel for Titles
+                  CarouselSlider(
+                    carouselController: _titleController,
+                    items: titles.map((title) => Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black
+                        ),
+                      ),
+                    )).toList(), 
+                    options: CarouselOptions(
+                      height: 40,
+                      enlargeCenterPage: true,
+                      viewportFraction: 0.5,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          picIndex = index;
+                        });
+                        _imageController.animateToPage(index);
+                      }
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
