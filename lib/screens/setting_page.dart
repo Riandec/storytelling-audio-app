98cc2b1845f116a50a8619c5e8187a7a8eaf96f8
Riandec <studpitch65@gmail.com>
2026-01-20 19:34:32 +0700

    fix(setting): change trailing icon to switch

--- lib/screens/setting_page.dart
+++ lib/screens/setting_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:storytelling_audio_app/screens/privacy_page.dart';
 
 class SettingPage extends StatefulWidget {
   const SettingPage({super.key});
@@ -9,6 +10,8 @@ class SettingPage extends StatefulWidget {
 
 class _SettingPageState extends State<SettingPage> {
   final searchController = SearchController();
+  bool notiEnabled = false;
+  bool darkModeEnabled = false;
 
   @override
   Widget build(BuildContext context) {
@@ -73,23 +76,63 @@ class _SettingPageState extends State<SettingPage> {
                       letterSpacing: 1
                     ),
                   ),
-                  trailing: Icon(Icons.arrow_forward_ios_rounded, color: Colors.black, size: 22),
+                  trailing: Switch(
+                    value: notiEnabled, 
+                    onChanged: (value){
+                      setState(() {
+                        notiEnabled = value;
+                      });
+                    },
+                    inactiveThumbColor: Colors.white,
+                    activeTrackColor: Color.fromRGBO(0, 85, 255, 1),
+                    inactiveTrackColor: const Color.fromARGB(255, 213, 208, 214),
+                    trackOutlineColor: WidgetStateProperty.resolveWith<Color?>((Set<WidgetState> states) {
+                      if (states.contains(WidgetState.selected)) {
+                        return Colors.transparent;
+                      }
+                      return Colors.transparent;
+                    }),
+                  ),
                   minTileHeight: 40,
                 ),
                 Divider(),
                 ListTile(
-                  leading: Icon(Icons.remove_red_eye_outlined, color: Colors.black),
+                  leading: Icon(Icons.dark_mode_outlined, color: Colors.black),
                   title: Text(
-                    'Appearance',
+                    'Dark mode',
                     style: TextStyle(
                       letterSpacing: 1
                     ),
                   ),
-                  trailing: Icon(Icons.arrow_forward_ios_rounded, color: Colors.black, size: 22),
+                  trailing: Switch(
+                    value: darkModeEnabled, 
+                    onChanged: (value){
+                      setState(() {
+                        darkModeEnabled = value;
+                      });
+                    },
+                    inactiveThumbColor: Colors.white,
+                    activeTrackColor: Color.fromRGBO(0, 85, 255, 1),
+                    inactiveTrackColor: const Color.fromARGB(255, 213, 208, 214),
+                    trackOutlineColor: WidgetStateProperty.resolveWith<Color?>((Set<WidgetState> states) {
+                      if (states.contains(WidgetState.selected)) {
+                        return Colors.transparent;
+                      }
+                      return Colors.transparent;
+                    }),
+                  ),
                   minTileHeight: 40,
                 ),
                 Divider(),
                 ListTile(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PrivacyPage()
+                      )
+                    );
+                  },
                   leading: Icon(Icons.lock_outline_rounded, color: Colors.black),
                   title: Text(
                     'Privacy and Security',
